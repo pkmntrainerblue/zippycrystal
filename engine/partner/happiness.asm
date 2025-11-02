db7ef7a3054eb41e16b04dc508f33f756d7afa5e
Blue <combats-jicama.6c@icloud.com>
2025-11-02 12:37:26 +0200

    Update happiness.asm

--- engine/partner/happiness.asm
+++ engine/partner/happiness.asm
@@ -1,62 +1,64 @@
 GetPartnerPikachuHappiness:
-; returns first Partner Pikachu's happiness in party (any slot), else 0
-; copies name only in the success case
+; Returns happiness of the first Partner Pikachu in the party (any slot)
+; Returns 0 and copies no name if not found
+; Skips eggs
 
 	ld hl, wPartyMon1Species
 	ld de, PARTYMON_STRUCT_LENGTH
-	ld c, 0 ; slot counter (0-5)
+	ld b, PARTY_SIZE  ; 6
 
 .loop
-	ld a, [hl] ; species
-	and a    ; end of party? (species 0)
-	jr z, .not_partner
-	cp -1    ; or $ff terminator if used
-	jr z, .not_partner
+	ld a, [hl]        ; species
+	and a             ; 0 = empty slot
+	jr z, .next_slot
+	cp EGG
+	jr z, .next_slot
 
-	push hl
-	push bc
+	; Check if it's PIKACHU
+	cp PIKACHU
+	jr nz, .next_slot
+
+	; Save species for name
 	ld [wNamedObjectIndex], a
+
+	; Load form byte
+	push hl
 	ld bc, MON_FORM - MON_SPECIES
 	add hl, bc
 	ld a, [hl]
 	ld [wNamedObjectIndex+1], a
+	pop hl
 
-	; Is this Partner Pikachu?
-	ld a, LOW(SPECIES_PIKACHU_PARTNER)
-	cp [wNamedObjectIndex]
-	jr nz, .no_match
-	ld a, HIGH(SPECIES_PIKACHU_PARTNER)
-	cp [wNamedObjectIndex+1]
-	jr nz, .no_match
+	; Check if form == PARTNER_FORM (6)
+	cp PARTNER_FORM
+	jr nz, .next_slot
 
-	; It is Partner Pikachu – check egg and load happiness
+	; It's Partner Pikachu! Now check egg flag
+	push hl
 	ld bc, MON_IS_EGG - MON_FORM
 	add hl, bc
 	bit MON_IS_EGG_F, [hl]
-	jr nz, .no_match ; eggs have no happiness
+	pop hl
+	jr nz, .next_slot  ; eggs don't have happiness
 
+	; Load happiness
+	push hl
 	ld bc, MON_HAPPINESS - MON_IS_EGG
 	add hl, bc
 	ld a, [hl]
 	ldh [hScriptVar], a
+	pop hl
 
-	pop bc ; discard counter
-	pop hl ; discard struct start
+	; Success: copy name and exit
 	call GetPokemonName
 	farjp CopyPokemonName_Buffer1_Buffer3
 
-.no_match
-	pop bc
-	pop hl
-
-	; Advance to next mon
-	add hl, de
-	inc c
-	ld a, c
-	cp PARTY_SIZE
+.next_slot
+	add hl, de        ; next mon
+	dec b
 	jr nz, .loop
 
-.not_partner
+	; Not found: return 0
 	xor a
 	ldh [hScriptVar], a
 	ret
